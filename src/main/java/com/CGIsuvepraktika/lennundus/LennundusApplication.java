80c967987b44e6f072977e4cf451432a116c04d4
Auraplus5 <kevin.alak@ut.ee>
2025-03-10 12:58:33 +0200

    initial

---
+++ src/main/java/com/CGIsuvepraktika/lennundus/LennundusApplication.java
@@ -0,0 +1,13 @@
+package com.CGIsuvepraktika.lennundus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LennundusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LennundusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CGIsuvepraktika/lennundus/LennundusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CGIsuvepraktika.lennundus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LennundusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
